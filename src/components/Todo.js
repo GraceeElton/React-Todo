ea71d6449ee68d32206806ee2edeb6125d95ad80
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-04-06 16:06:23 -0600

    Almost Mvp

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+//each item.
+
+import React from "react";
+
+const Todo = (props) => {
+  return (
+    <div
+      className={`item${props.item.done ? " done" : ""}`}
+      onClick={() => props.toggleItem(props.item.id)}
+    >
+      <p>{props.item.name}</p>
+    </div>
+  );
+};
+
+export default Todo;
